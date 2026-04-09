b52e4c1c1e8d62291863072f819c956677b4a273
smessie <smessie@smessie.com>
2026-04-09 09:28:12 +0200

    chore: Use node 24 to run benchmarks

--- install-client.sh
+++ install-client.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 22
+nvm install 24
 
 npm install
 npm run build
